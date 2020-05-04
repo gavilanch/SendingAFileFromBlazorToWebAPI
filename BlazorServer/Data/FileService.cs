f7f44f0214f0c63ae0ea2dee1bca7206eacd0fd3
Felipe Gavilan <elmejor_887@hotmail.com>
2020-05-04 19:48:47 -0400

    Initial Commit

---
+++ BlazorServer/Data/FileService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Net.Http.Headers;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Data
+{
+    public class FileService
+    {
+        private readonly HttpClient httpClient;
+        private string urlBytes = "https://localhost:44329/api/files/bytes";
+        private string urlFormFile = "https://localhost:44329/api/files/formfile";
+
+        public FileService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task SendImageBase64(string base64)
+        {
+            if (base64 == null) { return; }
+            var data = new { TestProperty = "This is a test", FileBase64 = base64 };
+            var dataJson = JsonSerializer.Serialize(data);
+            var stringContent = new StringContent(dataJson, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(urlBytes, stringContent);
+            Console.WriteLine($"Sucess: {response.IsSuccessStatusCode}");
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+        public async Task SendImageIFormFile(string base64)
+        {
+            if (base64 == null) { return; }
+            var requestContent = new MultipartFormDataContent();
+            
+            var fileBytes = Convert.FromBase64String(base64);
+            var imageContent = new ByteArrayContent(fileBytes);
+            imageContent.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("image/jpeg");
+
+            var testNameContent = new StringContent("this is a test value");
+
+            requestContent.Add(testNameContent, "TestProperty");
+            requestContent.Add(imageContent, "file", "image.jpg");
+
+            var response = await httpClient.PostAsync(urlFormFile, requestContent);
+            Console.WriteLine($"Sucess: {response.IsSuccessStatusCode}");
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+    }
+}
---
+++ WebAPI/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController: ControllerBase
+    {
+        [HttpPost("formfile")]
+        public ActionResult PostIFormFile([FromForm] InputIFormFileDTO inputIFormFileDTO)
+        {
+            if (inputIFormFileDTO == null) { return BadRequest("all null"); }
+            if (inputIFormFileDTO.File == null) { return BadRequest("File null"); }
+            return Ok();
+        }
+
+        [HttpPost("bytes")]
+        public ActionResult PostBase64File(InputBase64DTO inputBase64DTO)
+        {
+            if (inputBase64DTO == null) { return BadRequest("all null"); }
+            if (inputBase64DTO.FileBase64 == null) { return BadRequest("File null"); }
+            return Ok();
+        }
+    }
+
+    public class InputIFormFileDTO
+    {
+        public string TestProperty { get; set; }
+        public IFormFile File { get; set; }
+    }
+
+    public class InputBase64DTO
+    {
+        public string TestProperty { get; set; }
+        public string FileBase64 { get; set; }
+    }
+}
